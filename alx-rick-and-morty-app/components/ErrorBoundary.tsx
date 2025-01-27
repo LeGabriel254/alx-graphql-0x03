3d8804ad991712b82f0501c40eeabe160ec60c41
Leon Gabriel <g73346920@gmail.com>
2025-01-27 13:02:07 +0300

    Chored:Log the error and error information to the console for debugging purposes.

---
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+
+interface State {
+  hasError: boolean;
+}
+//`children` is of type ReactNode, representing the child components wrapped by the ErrorBoundary.
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+// Define the ErrorBoundary class component that extends React.Component.
+// It uses the ErrorBoundaryProps interface for props and the State interface for state.
+class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
+  // Constructor method initializes the component's state.
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+// Static lifecycle method to update the state when an error is caught.
+  static getDerivedStateFromError(error: Error): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+     // Log the error and error information to the console for debugging purposes.
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+
+
+    if (this.state.hasError) {
+      // If an error has occurred, display an error message with a button to retry.
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button onClick={() => this.setState({ hasError: false })}>
+            Try again?
+          </button>
+        </div>
+      );
+    }
+ // If no error occurred, render the child components.
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
